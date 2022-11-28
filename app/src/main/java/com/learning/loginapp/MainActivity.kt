0188c228193f3fa31aa221f5d844437afd329906
cheetahmail007 <cheetahmail007@gmail.com>
2022-11-29 00:50:01 +0530

    Enabled View Binding

--- app/src/main/java/com/learning/loginapp/MainActivity.kt
+++ app/src/main/java/com/learning/loginapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.learning.loginapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learning.loginapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
